370445d4c02600b038f6dec617835b5ff60ed20e
John McCarthy <John McCarthy>
2025-10-02 20:42:49 -0400

    simulate_ab wip

--- simulate_ab.py
+++ simulate_ab.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def name_to_id(id_df, name):
+    player_id = id_df.loc[id_df["Name"] == name, "MLBAMID"]
+    # name = player_name.get("Name")
+    if player_id.empty:
+        return ''
+    else:
+        return player_id.iloc[0]
+
 # Read in 2021 file
 statcast21 = pd.read_csv("../Statcast_2021.csv")
+
+
 
 # get pitcher data
 pitcher_data = statcast21[statcast21[""]]
